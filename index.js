6ba28dd163a0f25476a902f4eb9bdda866b39b13
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2024-01-16 10:27:37 +0800

    added express-ws

--- index.js
+++ index.js
@@ -1,64 +1,18 @@
 const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const appWs = require('express-ws')(app);
 
-app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 1337;
 
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
-});
-
-app.get('/{}', (req, res) => {
-    res.sendFile(__dirname + "/client/index-sensor.html");
-});
-
-DEVICES = {};
-SENSORS = {};
-
-// SocketIO connection
-io.on('connection', socket => {
-    console.log('user connected');
-    socket.emit('start', 'Connected to server');
-
-    socket.id = Math.floor(Math.random()*1_000_000);
-
-    socket.on('p', () => {
-        socket.emit('pong');
-    });
-    
-    socket.on('device-connect', () => {
-        DEVICES[socket.id] = socket;
+app.ws('/', ws => {
+    ws.on('init', (id) => {
+        console.log(`Sensor ${id} has connected to server`);
     });
 
-    socket.on('sensor-connect', () => {
-        SENSORS[socket.id] = socket;
-    });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-
-        delete SENSORS[socket.id];
-        delete DEVICES[socket.id];
+    ws.on('data', data => {
+        console.log(`Data Received: ${data.map((key, value) => { return `${key}: ${value}\n`})}`);
     });
 });
 
-function executePerFrame() {
-    sensorIDs = [];
-    for (key in SENSORS) {
-        sensorID = SENSORS[key].id;
-        sensorIDs.push(sensorID);
-    }
-
-    for (key in DEVICES) {
-        device = DEVICES[key];
-        
-        device.emit('active-sensors', sensorIDs);
-    }
-}
-setInterval(executePerFrame, 100);
-
-server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log('node: SERVER STARTED');
 });
